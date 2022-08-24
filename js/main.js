a0036f38e9d5439e87579d3c92441b627b5c4657
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2022-08-24 20:20:49 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ $(function(){
 
  // body宽高
   // let cw = 1920,ch = 1080
-  let cw = 1000 ,ch = height0822
+  let cw = 1920 ,ch = height0822
   let body = document.querySelector('body')
   body.style.width = `${cw}px`
   body.style.height = `${ch}px`
