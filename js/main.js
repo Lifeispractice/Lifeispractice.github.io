9f52b62d1d82999acc6254297d6da0baecacf6ab
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2022-08-24 13:35:33 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var identification=0
 var width0822 = $(window).width()
 var height0822 = $(window).height()
-alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周三 11点51 `)
+alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周三 13点34 `)
 
 $(function(){
 	
