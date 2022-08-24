17f89c288962cb0727dd5327d83d488f17160fbc
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2022-08-24 09:42:57 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
   
 var width0822 = $(window).width()
 var height0822 = $(window).height()
-alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822} 更新时间 17-26 `)
+alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822} 更新时间 周三 9-41 `)
 
 
   // body宽高
