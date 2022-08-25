b2e609e6adf7cbbcb10e43e31e01d1f1751f5c80
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2022-08-25 10:22:16 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var identification=0
 var width0822 = $(window).width()
 var height0822 = $(window).height()
-alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周四 修改点 最后3个模块导航只留概述 头部大标题不对 `)
+alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周四 修改点 最后3个模块导航只留概述 头图大标题已修改  `)
 
 $(function(){
 	
