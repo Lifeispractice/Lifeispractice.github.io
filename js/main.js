15441bd277ae2d2590378ba2c8bce1d1a6b38424
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2022-08-26 11:00:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var identification=0
 var width0822 = $(window).width()
 var height0822 = $(window).height()
-// alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周四 修改点 最后3个模块导航只留概述 头图大标题已修改 产品解决方案左边导航无法点击已修复  `)
+alert(`当前网站信息 屏幕宽度 ${width0822} 屏幕高度 ${height0822}  更新时间 周五 早上问题已改  `)
 
 $(function(){
 	
